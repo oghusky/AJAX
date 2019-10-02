f7824bf6a76917066329c5e74d472db665a226ff
Philip Simmons <philiposimmons@gmail.com>
2019-10-02 13:38:38 -0400

    used ajax instead of xhr, updated css

--- index.js
+++ index.js
@@ -1,42 +1,88 @@
-const content =  document.querySelector("#content");
-const btn = document.querySelector('.btn');
+// variables
+const btn = document.querySelector("#submit-btn");
+const zipInput = document.querySelector("#zip-input");
 
+// boolean to see more info about city weather
+let visibility = false;
+let seeMoreOrLess;
 
-btn.addEventListener('click', getJoke);
-function getJoke(){
-	// access http request in a variable
-	const xhr = new XMLHttpRequest();
-	// url of API we are working with
-	const url = "https://api.chucknorris.io/jokes/random";
-	// cycles thru ready states and consoles them
-	xhr.onreadystatechange = function(){
-		// checks if ready state is a 4 and is status is 200
-		if(xhr.readyState == 4){
-			if(xhr.status == 200){
-			// returns response back as a text
-			const str = xhr.responseText;
-			// takes responseText in and returns as usable text
-			const obj = JSON.parse(str);
-			// takes content and puts it in the content div
-			content.innerHTML = obj.value + '<br/><img src = "'+obj.icon_url+'">';
-			}else{
-				output.innerHTML = "Error";
-			}
+// date object
+const date = new Date();
+document.querySelector("#month").textContent = date.getMonth() + 1;
+document.querySelector("#day").textContent = date.getDate();
+document.querySelector("#year").textContent = date.getFullYear();
+// event listeners
+
+btn.addEventListener("click", getWeather);
+$(document).on("click", ".see-more-info-btn", showMoreCityInfo)
+// document.addEventListener("keypress", submitCity);
+
+// set state of text in "SEE MORE" button
+// gets weather info on click
+function getWeather(e) {
+	e.preventDefault();
+	// set city variable to input value
+	const city = zipInput.value;
+	// ajax call
+	if (city !== "") {
+		document.querySelector(".weather-container").textContent = "";
+		try {
+			$.ajax({
+				method: "GET",
+				url: `https://api.openweathermap.org/data/2.5/weather?zip=${city}
+		&units=imperial&appid=5fd23d240fe6ff93cb60e9314a69f456`
+			}).then(res => {
+				console.log(res);
+				const weatherInfoDiv = document.createElement("div");
+				weatherInfoDiv.classList.add("card", "px-3", "py-3", "my-3");
+				if (res.cod === 200) {
+					// get icon
+					const iconCode = res.weather[0].icon;
+					const iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+					// div to show weather info
+					weatherInfoDiv.innerHTML = `
+			<h3 class="card-title mb-1">${res.name}, ${res.sys.country} ${city}<span class="text-right"><img src=${iconUrl} alt="${res.name}, ${res.sys.country}"/></span></h3>
+			<div class="card-body mb-1">
+			<p><strong>Temperature:</strong> ${res.main.temp} &#8457</p>
+				<p><strong>Humidity:</strong> ${res.main.humidity}%</p>
+				<p class="text-right">
+				<button class="btn-primary see-more-info-btn">SEE <span class="show-more">MORE</span> <i class="fas fa-plus"></i></i> </button>
+				</p>
+				<div class="row see-more-info-div my-0">
+				<p class="col-sm-6"> <strong>Max Temp:</strong> ${res.main.temp_max} &#8457</p>
+					<p class="col-sm-6"> <strong>Min Temp:</strong> ${res.main.temp_min} &#8457</p>
+					<p class="col-sm-6"> <strong>Forecast:</strong> ${res.weather[0].main}</p>
+					<p class="col-sm-6"> <strong>Wind Speed</strong>: ${res.wind.speed} mph</p>
+					</div>
+					</div>
+			`
+					document.querySelector(".weather-container").append(weatherInfoDiv);
+					zipInput.value = "";
+					$(".see-more-info-div").hide();
+				} else if (res.cod !== 200) {
+					weatherInfoDiv.textContent = "Please Enter Valid Zip Code";
+					document.querySelector(".weather-container").append(weatherInfoDiv);
+				}
+			})
+		} catch (err) {
+			console.log(err);
 		}
 	}
-	// have to specify the method of request in "Open"
-	xhr.open("GET", url);
-	// sends the http request back to page. ready state show up 
-	// as a 4
-	xhr.send();
-	// progress info
-	xhr.addEventListener("progress", callBackfn);
-	// time stamp info
-	xhr.addEventListener("load", callBackfn);
-	// if we return an error
-	xhr.addEventListener("error", callBackfn);
 }
 
-function callBackfn(e){
-	console.log(e);
+
+function showMoreCityInfo(e) {
+	e.preventDefault();
+	visibility = !visibility;
+	e.target.value = visibility;
+	if (e.target.value === "true") {
+		$(".show-more").text("MORE");
+		$(".see-more-info-div").slideUp(500);
+	} else {
+		$(".show-more").text("LESS");
+		$(".see-more-info-div").slideDown(500);
+	}
 }
+
+
+
